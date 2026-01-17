42fa15b0ea35f8c35e19ebe9216cd08073b71b37
Siti Syahda Rana Areta <reta.syahda@nawatech.co>
2026-01-18 05:17:18 +0700

    feat: login & register with laravel blade

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // $products = Product::latest()->take(8)->get();
+    // return view('user.home', compact('products'));
+    return view('user.home');
+})->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
